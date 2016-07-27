545edf75145140359d562ce55fedc230ff0b4e9c
Nikita Bakaev <ya@nbakaev.ru>
2016-07-27 13:41:46 +0300

    delete aspect deps

--- src/test/java/com/biqasoft/microservice/communicator/interfaceimpl/StartApplicationTest.java
+++ src/test/java/com/biqasoft/microservice/communicator/interfaceimpl/StartApplicationTest.java
@@ -10,12 +10,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @ComponentScan(value = "com.biqasoft")
 @Configuration
-@EnableAspectJAutoProxy
 @EnableAutoConfiguration
 @EnableMicroserviceCommunicator
 public class StartApplicationTest {
