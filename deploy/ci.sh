159c1dfb254be05bcf170382fdd3f784b48e7289
Nikita Bakaev <ya@nbakaev.ru>
2016-07-31 12:35:50 +0300

    travis ci

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -10,7 +10,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-RELEASE+build.$TRAVIS_JOB_ID
 
     # deploy to maven binary repo
-    mvn deploy --settings $BASE_DIR/deploy/settings.xml -DperformRelease=true -DskipTests=true -Dmaven.javadoc.skip=true
+    mvn package --settings $BASE_DIR/deploy/settings.xml -DperformRelease=true -Dmaven.javadoc.skip=true -Dgpg.skip=true
     PROJECT_VERSION="`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null |grep -Ev '(^\[|Download\w+:)'`"
 
     echo "PROJECT_VERSION is $PROJECT_VERSION"
