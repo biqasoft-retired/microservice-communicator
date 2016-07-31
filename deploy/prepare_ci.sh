bcf0625f5df84275cfb3fa2216a3ccf8bd58607d
Nikita Bakaev <ya@nbakaev.ru>
2016-07-31 12:52:56 +0300

    travis ci

--- deploy/prepare_ci.sh
+++ deploy/prepare_ci.sh
@@ -8,7 +8,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
 
     # run consul for tests
     docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h node1 progrium/consul -server -bootstrap -ui-dir /ui
-    docker run -d -e "spring.cloud.consul.host=localhost" --net=host biqasoft/microservice-communicator-demo-server:latest
+    docker run -d -e "spring_cloud_consul_host=localhost" --net=host biqasoft/microservice-communicator-demo-server:latest
 
 # end build
     exit $?
