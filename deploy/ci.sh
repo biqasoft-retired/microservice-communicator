feb81730e5c1edbdcc0d1d2ddf97e46dd148478e
NBakaev <ya@nbakaev.ru>
2016-08-27 16:34:07 +0300

    use travis ci matrix

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -10,6 +10,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
 #    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-RELEASE+build.$TRAVIS_JOB_ID
 
     # java 9 test
+    if [[ $JAVA_VERSION == "openjdk9" ]]; then
 
     # only ubuntu 16.04 have in repo early build jdk9
     # sudo apt-get update & sudo apt install openjdk-9-jdk
@@ -18,11 +19,13 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     sudo mv jdk-9 java-9-openjdk-amd64
     sudo mv java-9-openjdk-amd64 /usr/lib/jvm
     env MAVEN_SKIP_RC="true" JAVA_HOME="/usr/lib/jvm/java-9-openjdk-amd64" JRE_HOME="/usr/lib/jvm/java-9-openjdk-amd64" mvn package -Pjdk9 -Djacoco.skip=true
-
     mvn clean
+    fi
 
+    if [[ $JAVA_VERSION == "openjdk8" ]]; then
     # deploy to maven binary repo
     mvn package --settings $BASE_DIR/deploy/settings.xml -DperformRelease=true -Dmaven.javadoc.skip=true -Dgpg.skip=true
+    fi
 
 #    PROJECT_VERSION="`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version 2> /dev/null |grep -Ev '(^\[|Download\w+:)'`"
 #    echo "PROJECT_VERSION is $PROJECT_VERSION"
